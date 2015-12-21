e8d0eaa6fff464c591da255e2f5a5595f0b19cef
JGrycel <maddrummer10@gmail.com>
2015-12-21 15:21:25 -0500

    Update Auto12_14.java
    created motor and sensor objects for autonomous mode, and created class structure to extend linear op

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/Autonomous/Auto12_14.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/Autonomous/Auto12_14.java
@@ -1,2 +1,40 @@
-public class Teleop12_14 extends LinearOpMode {
+package com.qualcomm.ftcrobotcontroller.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+public class Auto12_14 extends LinearOpMode{
+
+    DcMotor motor_arm, motor_winch_in, motor_winch_out, driveLeft, driveRight;
+    UltrasonicSensor distanceSensor;
+    ColorSensor colorSensor;
+    GyroSensor gyro;
+
+    public void runOpMode()throws InterruptedException {
+
+        motor_arm = hardwareMap.dcMotor.get("motor_arm");
+        motor_winch_in = hardwareMap.dcMotor.get("motor_winch_in");
+        motor_winch_out = hardwareMap.dcMotor.get("motor_winch_out");
+        driveLeft = hardwareMap.dcMotor.get("driveLeft");
+        driveRight = hardwareMap.dcMotor.get("driveRight");
+        distanceSensor = hardwareMap.ultrasonicSensor.get("distanceSensor");
+        colorSensor = hardwareMap.colorSensor.get("colorSensor");
+        gyro = hardwareMap.gyroSensor.get("gyro");
+
+
+        driveLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        motor_arm.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        driveRight.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        driveLeft.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+
+        waitForStart();
+
+    }
+
 }
