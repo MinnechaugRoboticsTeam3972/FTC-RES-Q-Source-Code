6bd729fad2101b1fe7195f2d00e466ec480973ac
JGrycel <maddrummer10@gmail.com>
2015-12-21 22:41:15 -0500

    Update Teleop12_14.java
    objects created for each element and comments added

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Teleop12_14.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Teleop12_14.java
@@ -2,21 +2,44 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.ftcrobotcontroller.RobotFunctions.HangingArm;
 
 public class Teleop12_14 extends OpMode {
 
-    DcMotor driveLeft;
-    DcMotor driveRight;
+    DcMotor motor_arm, motor_winch_in, motor_winch_out, driveLeft, driveRight;
+    UltrasonicSensor distanceSensor;
+    ColorSensor colorSensor;
+    GyroSensor gyro;
+    HangingArm hangingArm;
 
     public void init() {
+        //create sensor and motor objects for robot parts
+        motor_arm = hardwareMap.dcMotor.get("motor_arm");
+        motor_winch_in = hardwareMap.dcMotor.get("motor_winch_in");
+        motor_winch_out = hardwareMap.dcMotor.get("motor_winch_out");
         driveLeft = hardwareMap.dcMotor.get("driveLeft");
         driveRight = hardwareMap.dcMotor.get("driveRight");
+        distanceSensor = hardwareMap.ultrasonicSensor.get("distanceSensor");
+        colorSensor = hardwareMap.colorSensor.get("colorSensor");
+        gyro = hardwareMap.gyroSensor.get("gyro");
 
         driveLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        //this motor will have to run at a contstant velocity
+        motor_arm.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+
+        //create hanging arm motor cluster for whole arm motions
+        hangingArm = new HangingArm(motor_arm, motor_winch_in, motor_winch_out);
     }
 
     public void loop() {
+
+        //controller 1 controls drive with joystick
         double rightDrivePower = -gamepad1.right_stick_y;
         double leftDrivePower = -gamepad1.left_stick_y;
 
@@ -25,5 +48,8 @@ public class Teleop12_14 extends OpMode {
 
         driveLeft.setPower(leftDrivePower);
         driveRight.setPower(rightDrivePower);
+
+        //controller 2 controls lifting arm
+
     }
 }
