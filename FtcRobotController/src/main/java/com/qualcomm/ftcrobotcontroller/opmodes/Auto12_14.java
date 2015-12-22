19a8e6d60bd22a3d19ed9d7392c67c6d76e9f054
JGrycel <maddrummer10@gmail.com>
2015-12-22 16:36:42 -0500

    Update Auto12_14.java
    removed servo import

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Auto12_14.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Auto12_14.java
@@ -2,7 +2,6 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
