21bca4525030287003dbb8c99f8aa8315985b1ee
JGrycel <maddrummer10@gmail.com>
2015-12-22 23:36:11 -0500

    Update NxtGyro.java
    imports and constructs ElapsedTime

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/RobotFunctions/NxtGyro.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/RobotFunctions/NxtGyro.java
@@ -3,16 +3,18 @@ package com.qualcomm.ftcrobotcontroller.RobotFunctions;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-class NxtGyro {
+public class NxtGyro {
 
     GyroSensor gyroSensor;
+    ElapsedTime timer;
 
-    public NxtGyro(GyroSensor gyro){
+    public NxtGyro(GyroSensor gyro, ElapsedTime time){
         gyroSensor = gyro;
+        timer = time;
     }
 
     public double getHeading(){
 
-        return 2;
+        return 1;
     }
 }
