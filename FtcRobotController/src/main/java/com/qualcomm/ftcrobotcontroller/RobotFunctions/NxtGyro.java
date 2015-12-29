86a35aba75987975fa55af8e5a9f28afa8d98151
JGrycel <maddrummer10@gmail.com>
2015-12-29 01:10:50 -0500

    Update NxtGyro.java
    Large update to file

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/RobotFunctions/NxtGyro.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/RobotFunctions/NxtGyro.java
@@ -3,18 +3,27 @@ package com.qualcomm.ftcrobotcontroller.RobotFunctions;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class NxtGyro {
+public class NxtGyro{
 
     GyroSensor gyroSensor;
     ElapsedTime timer;
+    double heading = 0;
 
     public NxtGyro(GyroSensor gyro, ElapsedTime time){
         gyroSensor = gyro;
         timer = time;
     }
 
+    //returns heading in degrees
     public double getHeading(){
+        double temp = gyroSensor.getRotation();
+        temp *= timer.time();
+        timer.reset();
+        heading += temp;
+        return heading;
+    }
 
-        return 1;
+    public void resetHeading(){
+        heading = 0;
     }
 }
