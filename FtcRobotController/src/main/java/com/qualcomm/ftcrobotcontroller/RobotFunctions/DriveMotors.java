9daa9bc2eca56315c257917c276226258fd3e5c3
JGrycel <maddrummer10@gmail.com>
2016-01-04 22:24:15 -0500

    Update DriveMotors.java
    large update

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/RobotFunctions/DriveMotors.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/RobotFunctions/DriveMotors.java
@@ -5,51 +5,82 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class DriveMotors {
-    DcMotor driveLeft, driveRight;
-    NxtGyro nxtGyro;
-    ElapsedTime timer;
+    private DcMotor driveLeft, driveRight;
+    private NxtGyro nxtGyro;
+    private ElapsedTime timer;
+    private double distance;
 
+    //DriveMotors Constructor
     public DriveMotors(DcMotor left, DcMotor right, NxtGyro gyro, ElapsedTime time){
         driveLeft = left;
         driveRight = right;
         nxtGyro = gyro;
         timer = time;
+        distance = 0;
     }
 
-    public void DriveForwards(long time) throws InterruptedException{
+    //Drive robot forwards
+    public void DriveForwards() {
         this.checkMode();
         driveLeft.setPower(100);
         driveRight.setPower(100);
-        Thread.sleep(time);
     }
 
-    public void DriveBackwards(long time) throws InterruptedException{
+    //Drive robot backwards
+    public void DriveBackwards() {
         this.checkMode();
         driveLeft.setPower(-100);
         driveRight.setPower(-100);
-        Thread.sleep(time);
     }
 
+    //Turn robot to the right until desired angle is reached
     public void turnRight(double angle){
         this.checkMode();
         driveLeft.setPower(100);
         driveRight.setPower(-100);
         timer.reset();
-        while(nxtGyro.getHeading() < 90){}
-        driveLeft.setPower(0);
-        driveRight.setPower(0);
+        while(nxtGyro.getHeading() < angle){
+            //wait until turn is complete
+        }
+        nxtGyro.resetHeading();
+        this.stop();
     }
 
+    //Turn robot to the left until desired angle is reached
     public void turnLeft(double angle){
         this.checkMode();
         driveLeft.setPower(-100);
         driveRight.setPower(100);
         timer.reset();
-        while(nxtGyro.getHeading() < 90){}
+        while(nxtGyro.getHeading() < angle){
+            //wait until turn is complete
+        }
+        nxtGyro.resetHeading();
+        this.stop();
+    }
+
+    //Get the total distance traveled by the robot since last reset
+    public double getDistance(){
+        distance +=  driveRight.getCurrentPosition() * 23.8125;
+        return distance;
+    }
+
+    //Reset both the encoder distance and instance variable value
+    //WARNING!! This method should only be used when drive motors are not running to ensure accurate calibration
+    public void resetDistance(){
+        distance = 0;
+        driveLeft.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        driveRight.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        this.checkMode();
+    }
+
+    //Stop both drive motors
+    public void stop(){
         driveLeft.setPower(0);
         driveRight.setPower(0);
     }
 
+    //check the encoder state of both motors; if in autonomous mode, set motors to run with encoders
     private void checkMode(){
         if(driveLeft.getMode() != DcMotorController.RunMode.RUN_USING_ENCODERS){
             driveLeft.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
