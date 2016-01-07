6eb0c0bb4fa1e20a8d3bb69d7f9a6a8b6774f4fa
JGrycel <maddrummer10@gmail.com>
2016-01-06 22:21:26 -0500

    Update Auto_Blue_Mountain.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Auto_Blue_Mountain.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Auto_Blue_Mountain.java
@@ -3,63 +3,60 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.LightSensor;
-import com.qualcomm.robotcore.hardware.UltrasonicSensor;
-import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.ftcrobotcontroller.RobotFunctions.DriveMotors;
+import com.qualcomm.ftcrobotcontroller.RobotFunctions.advUltrasonicSensor;
 
-public class Auto_Blue_Mountain extends LinearOpMode {
+public class Auto_Blue_Floor extends LinearOpMode {
 
-    DcMotor motor_arm, motor_winch_in, motor_winch_out, driveLeft, driveRight;
-    LightSensor lightSensor;
-    UltrasonicSensor frontDistance;
-    UltrasonicSensor backDistance;
-    UltrasonicSensor floorDistance;
-    GyroSensor gyro;
+    DcMotor motor_arm, motor_winch_1, motor_winch_2, driveLeft, driveRight;
+    advUltrasonicSensor frontDistance, backDistance, floorDistance;
     ElapsedTime timer;
     DriveMotors driveMotors;
-    final boolean isAutonomous = true;
-    double distance = 0;
+    DcMotorController drive;
 
     public void runOpMode()throws InterruptedException {
-
-        //create sensor and motor objects for robot parts
-        motor_arm = hardwareMap.dcMotor.get("motor_arm");
-        motor_winch_in = hardwareMap.dcMotor.get("motor_winch_in");
-        motor_winch_out = hardwareMap.dcMotor.get("motor_winch_out");
-        driveLeft = hardwareMap.dcMotor.get("driveLeft");
-        driveRight = hardwareMap.dcMotor.get("driveRight");
-        lightSensor = hardwareMap.lightSensor.get("lightSensor");
-        frontDistance = hardwareMap.ultrasonicSensor.get("frontDistance");
-        backDistance = hardwareMap.ultrasonicSensor.get("backDistance");
-        floorDistance = hardwareMap.ultrasonicSensor.get("floorDistance");
-        gyro = hardwareMap.gyroSensor.get("gyro");
 
         //create object to keep track of time; will be sent to nxtGyro for finding heading
         timer = new ElapsedTime();
 
+        //create sensor and motor objects for robot parts
+        drive = hardwareMap.dcMotorController.get("drive motors");
+        driveLeft = hardwareMap.dcMotor.get("driveLeft");
+        driveRight = hardwareMap.dcMotor.get("driveRight");
+        frontDistance = new advUltrasonicSensor(hardwareMap.ultrasonicSensor.get("frontDistance"), timer);
+        backDistance = new advUltrasonicSensor(hardwareMap.ultrasonicSensor.get("backDistance"), timer);
+        floorDistance = new advUltrasonicSensor(hardwareMap.ultrasonicSensor.get("floorDistance"),timer);
+
         //create motor cluster for drive motors;
-        driveMotors = new DriveMotors(driveLeft, driveRight, timer);
+        driveMotors = new DriveMotors(driveLeft, driveRight, drive);
 
         driveLeft.setDirection(DcMotor.Direction.REVERSE);
-
-        //these motors will have to run at constant speeds
-        motor_arm.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        driveRight.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        driveLeft.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
         waitOneFullHardwareCycle();
 
         waitForStart();
 
-        //drive leg of triangle adjacent to start
-        driveMotors.DriveForwards();
-        while(driveMotors.getDistance() < 213.36){
-            if()
+        //drive forwards for length of leg of triangle; check for inconsistencies in movement
+        driveMotors.driveForwards();
+        while(driveMotors.getDistance() < Triangle.bottomLeg){
+            checkDrive(backDistance, driveMotors);
         }
+        driveMotors.stop();
+        driveMotors.resetDistance();
 
-        //one encoder turn = 23.8125 centimeters (drive motors)
+        //turn Pi/2 radians to the right
+        driveMotors.turnRight(Math.PI / 2.0);
+    }
 
+    static void checkDrive(advUltrasonicSensor sensor, DriveMotors motors)throws InterruptedException{
+        sensor.calculateNewRate();
+        Thread.sleep(500);
+        sensor.calculateNewRate();
+        if(sensor.getRateDifference() > 1 || sensor.getRateDifference() < -1 ){
+            motors.stop();
+            Thread.sleep(1000);
+            motors.driveForwards();
+        }
     }
 }
